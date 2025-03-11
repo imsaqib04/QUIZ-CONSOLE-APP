09651d696cf65e621d004a709d935ffcc235c18b
saqib <saqibansari2011@gmail.com>
2025-03-11 15:09:29 +0530

    Quiz-app-v1

---
+++ Quiz-Console-App/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+
+        QuestionService questionService = new QuestionService ();
+        questionService.playQuiz ();
+        questionService.printScore ();
+    }
+}
